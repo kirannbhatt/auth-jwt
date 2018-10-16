df98aecad5dc7f1c1d0d861cb13476d349734867
Kiran Bhatt <kb_43@outlook.com>
2018-10-16 11:24:24 +0545

    apiRouter added

--- server/server.js
+++ server/server.js
@@ -1,8 +1,23 @@
 import express from 'express'
+import bodyParser from 'body-parser'
+import logger from 'morgan'
+import path from 'path'
+import { } from 'dotenv/config'
+import '@babel/polyfill'
+
+import router from './routes/apiRoutes'
+
 const app = express()
 
+app.use(logger('dev'))
+app.use(express.json())
+app.use(bodyParser.json())
+app.use(bodyParser({extended: false}))
+
+app.use('/', router);
+
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.sendFile(path.join(__dirname + '/public/index.html'));
 })
 
 export default app;
