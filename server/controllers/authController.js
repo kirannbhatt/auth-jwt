292b126bd8da3a3e56b551ded8d242b73dab5c61
Kiran Bhatt <kb_43@outlook.com>
2018-10-16 14:45:21 +0545

    login and verify function added

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,6 +1,7 @@
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 import User from '../models/user'
+import tokenDecoder from '../middlewares/tokenDecoder'
 
 const hashRounds = 10;
 
@@ -19,7 +20,7 @@ const authController = {
           email: req.body.email
         },
       });
-      if(user) {
+      if (user) {
         return res.status(403).json({ message: 'User already exists' });
       }
       const newUser = await User.create(
@@ -29,12 +30,40 @@ const authController = {
         }
       );
 
-      if(newUser){
-        return res.status(200).json({ message: 'User created successfully'});
+      if (newUser) {
+        return res.status(200).json({ message: 'User created successfully' });
       }
-      
-    } catch(error) {
-      res.json({ message: 'Error'});
+
+    } catch (error) {
+      res.json({ message: 'Error' });
+    }
+  },
+  login: async (req, res) => {
+    try {
+      const user = await User.findOne({
+        email: req.body.email
+      });
+      const payload = {
+        name: user.email,
+        _uid: user._uid,
+        date: Date.now()
+      };
+
+      if (user) {
+        if (bcrypt.compareSync(req.body.password, user.password)) {
+          const token = signToken(payload);
+          res.status(200).json({ message: 'Welcome to the world of JS', Token: token });
+        }
+      }
+    } catch (error) {
+      res.status(403).json({ message: 'You cannot pass!!!' });
+    }
+  },
+  verify: async (req, res) => {
+    if(tokenDecoder.verify(req.body.token)) {
+      res.status(200);
+    } else {
+      res.status(401);
     }
   }
 }
